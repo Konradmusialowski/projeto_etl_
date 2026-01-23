a66a3c9c1428c3d2f01c7cba0a07ec767fa9de4e
konradmusialowski <137570130+Konradmusialowski@users.noreply.github.com>
2026-01-23 21:54:54 +0000

    commit arquivos da pasta notebok e sql

---
+++ M1S1h-main/M1S10-main/notebooks/notebook.py
@@ -0,0 +1,111 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use("ggplot")
+
+df = pd.read_csv("consulta.csv", sep=";")
+df2=pd.read_csv("consulta2.csv", sep=";")
+# Visualizar as primeiras linhas
+print("\n📊 Estatísticas - Ano")
+display(df['ano'].describe())
+
+print("\n📊 Estatísticas - Mês")
+display(df['mes'].describe())
+
+print("\n📊 Estatísticas - Total Entrada")
+display(df['total_entrada'].describe())
+
+display(df.head())
+
+mov = df['total_entrada'].values
+
+media = np.mean(mov)
+variancia = np.var(mov)
+desvio = np.std(mov)
+
+
+print("\n📊 Estatísticas - Ano")
+display(df['ano'].describe())
+
+print("\n📊 Estatísticas - Mês")
+display(df['mes'].describe())
+
+print("\n📊 Estatísticas - Total Entrada")
+display(df['total_entrada'].describe())
+
+
+print("📌 rank")
+ranking_centros = (
+    df.groupby('nome_centro_distribuicao')['total_entrada']
+    .sum()
+    .reset_index()
+    .sort_values(by='total_entrada', ascending=False)
+)
+
+print("📌 Ranking de Centros por Volume de Entrada")
+display(ranking_centros)
+
+plt.figure(figsize=(10,6))
+sns.barplot(
+    data=ranking_centros,
+    x='total_entrada',
+    y='nome_centro_distribuicao'
+)
+
+plt.title("Total de Entrada por Centro de Distribuição")
+plt.xlabel("Total de Entrada")
+plt.ylabel("Centro de Distribuição")
+plt.tight_layout()
+plt.show()
+
+
+
+
+ranking_produtos = (
+    df.groupby('codigo_produto')['total_entrada']
+    .sum()
+    .reset_index()
+    .sort_values(by='total_entrada', ascending=False)
+)
+
+print("📌 Ranking de Produtos por Volume de Entrada")
+display(ranking_produtos)
+
+plt.figure(figsize=(10,6))
+sns.barplot(
+    data=ranking_produtos,
+    x='total_entrada',
+    y='codigo_produto'
+)
+
+plt.title("Produtos com Maior Volume de Entrada")
+plt.xlabel("Total de Entrada")
+plt.ylabel("Código do Produto")
+plt.tight_layout()
+plt.show()
+
+entrada_por_mes = (
+    df.groupby(['ano','mes'])['total_entrada']
+    .sum()
+    .reset_index()
+)
+
+display(entrada_por_mes)
+
+plt.figure(figsize=(10,6))
+sns.lineplot(
+    data=entrada_por_mes,
+    x='mes',
+    y='total_entrada',
+    hue='ano',
+    marker='o'
+)
+
+plt.title("Evolução Mensal das Entradas")
+plt.xlabel("Mês")
+plt.ylabel("Total de Entrada")
+plt.tight_layout()
+plt.show()
--- M1S1h-main/M1S10-main/sql/consulta2.sql
+++ M1S1h-main/M1S10-main/sql/consulta2.sql
@@ -1,15 +1,16 @@
-
-SELECT distinct
+SELECT
     d.nome_centro_distribuicao,
     f.codigo_produto,
-    SUM(f.quantidade_entrada) AS total_entrada,
-    SUM(f.quantidade_saida)   AS total_saida
+    EXTRACT(YEAR FROM f.data_movimentacao) AS ano,
+    SUM(f.quantidade_entrada) AS total_entrada
 FROM dw.dim_fact_movimentacao_estoque f
 JOIN dw.dim_centro_distribuicao d
     ON f.cd_id = d.cd_id
 GROUP BY
     d.nome_centro_distribuicao,
-    f.codigo_produto
+    f.codigo_produto,
+    EXTRACT(YEAR FROM f.data_movimentacao)
 ORDER BY
+    ano,
     d.nome_centro_distribuicao,
-    total_saida DESC;
+    total_entrada DESC;
